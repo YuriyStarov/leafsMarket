4736c4343c2c947b580ac26ade6580b17f6e5679
Yurk <Starovman@ukr.net>
2023-05-27 07:24:59 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MainComponent } from './main/main.component';
+import { LotsComponent } from './lots/lots.component';
+import { ShopComponent } from './shop/shop.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes =[
+  { path: '', component: MainComponent},
+  { path: 'lots', component: LotsComponent},
+  { path: 'shop', component: ShopComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LotsComponent,
+    ShopComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lots/lots.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lots',
+  templateUrl: './lots.component.html',
+  styleUrls: ['./lots.component.scss']
+})
+export class LotsComponent {
+
+}
