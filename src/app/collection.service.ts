b183ec55f36177adea90abbf35398d92e5d308e9
Yurk <Starovman@ukr.net>
2023-05-27 12:04:56 +0300

    filter process

--- src/app/collection.service.ts
+++ src/app/collection.service.ts
@@ -24,4 +24,8 @@ export class CollectionService {
 
   }
 
+  returnCollection () {
+    return this.leafsCollection
+  }
+
 }
--- src/app/main/lots-list/lots-list.component.ts
+++ src/app/main/lots-list/lots-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CollectionService } from 'src/app/collection.service';
 
 @Component({
   selector: 'app-lots-list',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./lots-list.component.scss']
 })
 export class LotsListComponent {
+
+  @Input() regular: string | undefined;
+  
+
+
+  constructor (private dataService: CollectionService) {
+    
+  }
+
+  render (item: object): string | void  {
+    let bool: string;
+    if (!this.regular) {
+      bool = 'block';
+      return bool
+    } 
+    else {
+      if ('name' in item) {
+        const regexp = new RegExp(this.regular!)
+        if (typeof item.name === 'string') {
+          const itemValue: string = item.name;
+          if(regexp.test(itemValue)) {
+            bool = 'block';
+          }
+          else {
+            bool = 'none';
+          };
+          return bool;
+        }
+      };
+    };
+  }
 
 }
